ee1d1db50904cc108b9f2721acebc4a90c94ead3
莊汶樺 <suzi570451@gmail.com>
2020-04-14 16:32:43 +0800

    new

--- iHealthNote/ViewController.swift
+++ iHealthNote/ViewController.swift
@@ -13,8 +13,45 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        //註釋
+        //變數
+        var index : Int = 1
+        //常數
+        let opcode : Int = 2
+        
+        let result = index + opcode
+        
+        print("結果:\(result)")
+        
+        if index == 2 {
+            print("2")
+        }
+        
+        else{
+            print("\(index)")
+        }
+        
+        var 橘子 : Int = 0
+        var 西瓜 : Int = 0
+        
+        let 到超市 : Bool = true
+        let 看到西瓜 = true
+        
+        if 到超市{
+            
+            if 看到西瓜
+            {
+                橘子 = 3
+                西瓜 = 1
+            }
+            
+        }
+        else {橘子 = 3}
+        
+        
+        
     }
 
-
+    
 }
 
